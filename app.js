f4dbd55236bdd9231554fdb3a428445b3e03f1c9
ShelbyE <60080830+Shelby219@users.noreply.github.com>
2020-09-21 12:46:11 +1000

    set up bundle browserify

--- app.js
+++ app.js
@@ -2,34 +2,6 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 
 
-
-async function makeGetRequest(url) {
-
-    let res = await axios.get(url);
-  
-    let data = res.data;
-    const $ = cheerio.load(data);
-    // Print the full HTML
-    //console.log(`Site HTML: ${$.html()}\n\n`)
-
-    // Print some specific page content
-    console.log(`First h1 tag: ${$('h1').text()}`)
-  }
-  
-makeGetRequest("https://goo.gl/MV8Tkh");
-
-
-
-const axios = require('axios')
-const cheerio = require('cheerio')
-
-
-
-const axios = require('axios')
-const cheerio = require('cheerio')
-
-
-
 async function makeGetRequest(url) {
 
     let res = await axios.get(url);
@@ -55,20 +27,6 @@ makeGetRequest("https://goo.gl/MV8Tkh");
 
 
 
-
-// function fetchHTML(url) {
-//     const data = axios.get(url)
-//    //.then(response => console.log(response.data))
-//    console.log(cheerio.load(data))
-// }
-
-// const $ = fetchHTML("https://goo.gl/MV8Tkh")
-
-// // Print the full HTML
-// console.log(`Site HTML: ${$.html()}\n\n`)
-
-// // Print some specific page content
-// console.log(`First h1 tag: ${$('h1').text()}`)
 
 
 
