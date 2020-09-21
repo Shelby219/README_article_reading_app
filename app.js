4364106d69e538e1e64026cac0eb1cafdc2b72f4
ShelbyE <60080830+Shelby219@users.noreply.github.com>
2020-09-21 16:48:37 +1000

    added play button

--- app.js
+++ app.js
@@ -27,100 +27,113 @@ async function makeGetRequest() {
   const $ = cheerio.load(data);
  
   // Print some specific page content
-  console.log(`First h1 tag: ${$('h1').text()}`) 
-  console.log(`pp`) 
+  var text2Speech = $('h1').text()
+  console.log(text2Speech) 
+  const textArticle = document.createElement("div");
+  const textnode = document.createTextNode(text2Speech); 
+  document.body.appendChild(textArticle);         // Create a text node
+  textArticle.appendChild(textnode); 
+  const playBut = document.createElement("BUTTON");
+  playBut.innerHTML = "Play Me";  
+  document.body.appendChild(playBut); 
+  return text2Speech;
 }
 
 myButton.addEventListener("click", makeGetRequest)
 
+const playButton =  document.getElementById('playBut');
+  if (typeof(playButton) != 'undefined' && playButton != null)
+  {
+    // Exists.
+    playButton.addEventListener("click", speakText)
+  }
 
 //https://medium.com/@ndaidong/zato-a-powerful-python-based-esb-solution-for-your-soa-5aef67114570
 
+        // Function invoked by button click
+  function speakText() {
+    console.log("pp") 
+          // Create the JSON parameters for getSynthesizeSpeechUrl
+          var speechParams = {
+              OutputFormat: "mp3",
+              SampleRate: "16000",
+              Text: "",
+              TextType: "text",
+              VoiceId: "Matthew"
+          };
 
-
-//     // Function invoked by button click
-//         function speakText() {
-//           // Create the JSON parameters for getSynthesizeSpeechUrl
-//           var speechParams = {
-//               OutputFormat: "mp3",
-//               SampleRate: "16000",
-//               Text: "",
-//               TextType: "text",
-//               VoiceId: "Matthew"
-//           };
-//           speechParams.Text = document.getElementById("textEntry").value;
+          speechParams.Text = text2Speech;
           
-//           // Create the Polly service object and presigner object
-//           var polly = new AWS.Polly({apiVersion: '2016-06-10'});
-//           var signer = new AWS.Polly.Presigner(speechParams, polly)
+          // Create the Polly service object and presigner object
+          var polly = new AWS.Polly({apiVersion: '2016-06-10'});
+          var signer = new AWS.Polly.Presigner(speechParams, polly)
       
-//           // Create presigned URL of synthesized speech file
-//           signer.getSynthesizeSpeechUrl(speechParams, function(error, url) {
-//           if (error) {
-//               document.getElementById('result').innerHTML = error;
-//           } else {
-//               document.getElementById('audioSource').src = url;
-//               document.getElementById('audioPlayback').load();
-//               document.getElementById('result').innerHTML = "Speech ready to play.";
+          // Create presigned URL of synthesized speech file
+          signer.getSynthesizeSpeechUrl(speechParams, function(error, url) {
+          if (error) {
+              document.getElementById('result').innerHTML = error;
+          } else {
+              document.getElementById('audioSource').src = url;
+              document.getElementById('audioPlayback').load();
+              document.getElementById('result').innerHTML = "Speech ready to play.";
+          }
+        });
+      }
+
+      
+
+
+
+
+
+
+
+
+// // Create an Polly client
+// const Polly = new AWS.Polly({
+//   signatureVersion: 'v4',
+//   region: 'ap-southeast-2'
+// });
+
+// const s3 = new AWS.S3({
+//   accessKeyId: process.env.AWS_ACCESS_KEY,
+//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+// });
+
+
+// const textToSpeechConverter = (params, configs) => {
+//   Polly.synthesizeSpeech(params, (err, data) => {
+//       if (err) {
+//           throw err;
+//       } else if (data) {
+//           if (data.AudioStream instanceof Buffer) {
+//               // Read content from the file
+//               const fileContent = fs.readFileSync(data.AudioStream);
+//                 // Setting up S3 upload parameters
+//               const params = {
+//                   Bucket: 'polly-test-11234', // pass your bucket name
+//                   Key: 'test.mp3', // file will be saved as testBucket/contacts.csv
+//                   Body: fileContent
+//               };
+//               // Uploading files to the bucket
+//               s3.upload(params, function(err, data) {
+//                   if (err) {
+//                       throw err;
+//                   }
+//                   console.log(`File uploaded successfully. ${data.Location}`);
+//               });
 //           }
-//         });
 //       }
-
-
-
-
-
-
-
-
-
-
-
-// // // Create an Polly client
-// // const Polly = new AWS.Polly({
-// //   signatureVersion: 'v4',
-// //   region: 'ap-southeast-2'
-// // });
-
-// // const s3 = new AWS.S3({
-// //   accessKeyId: process.env.AWS_ACCESS_KEY,
-// //   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-// // });
-
-
-// // const textToSpeechConverter = (params, configs) => {
-// //   Polly.synthesizeSpeech(params, (err, data) => {
-// //       if (err) {
-// //           throw err;
-// //       } else if (data) {
-// //           if (data.AudioStream instanceof Buffer) {
-// //               // Read content from the file
-// //               const fileContent = fs.readFileSync(data.AudioStream);
-// //                 // Setting up S3 upload parameters
-// //               const params = {
-// //                   Bucket: 'polly-test-11234', // pass your bucket name
-// //                   Key: 'test.mp3', // file will be saved as testBucket/contacts.csv
-// //                   Body: fileContent
-// //               };
-// //               // Uploading files to the bucket
-// //               s3.upload(params, function(err, data) {
-// //                   if (err) {
-// //                       throw err;
-// //                   }
-// //                   console.log(`File uploaded successfully. ${data.Location}`);
-// //               });
-// //           }
-// //       }
-// //   });
-// // };
-// //       let params = {
-// //           'Text': text4Input,
-// //           'OutputFormat': 'mp3',
-// //           'VoiceId': voiceId,
-// //           'TextType': 'ssml'
-// //       };
-// //       console.log(params);
-// //       textToSpeechConverter(params, {});
+//   });
+// };
+//       let params = {
+//           'Text': text4Input,
+//           'OutputFormat': 'mp3',
+//           'VoiceId': voiceId,
+//           'TextType': 'ssml'
+//       };
+//       console.log(params);
+//       textToSpeechConverter(params, {});
 
 
 
