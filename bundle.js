b454480ec21bcefda93d44f1e7a51c453259172b
ShelbyE <60080830+Shelby219@users.noreply.github.com>
2020-09-21 14:22:34 +1000

    trying to sort out event listener

--- bundle.js
+++ bundle.js
@@ -2,23 +2,33 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
 
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
-async function makeGetRequest(url) {
-
-    let res = await axios.get(url);
-  
-    let data = res.data;
-    const $ = cheerio.load(data);
-    // Print the full HTML
-    //console.log(`Site HTML: ${$.html()}\n\n`)
-
-    // Print some specific page content
-    console.log(`First h1 tag: ${$('h1').text()}`)
-  }
-  
-makeGetRequest("https://goo.gl/MV8Tkh");
+const myButton = document.getElementById("submit-button");
 
 
+//Get request the request URL
+async function makeGetRequest() {
+
+  let url = document.querySelector("searched-url");
+
+  let res = await axios.get(proxyurl + url);
+
+  let data = res.data;
+  const $ = cheerio.load(data);
+  // Print the full HTML
+  //console.log(`Site HTML: ${$.html()}\n\n`)
+
+  // Print some specific page content
+  console.log(`First h1 tag: ${$('h1').text()}`) 
+  console.log(`pp`) 
+}
+//makeGetRequest();
+
+myButton.addEventListener("click", makeGetRequest)
+//makeGetRequest("https://goo.gl/MV8Tkh");
+
+//https://medium.com/@ndaidong/zato-a-powerful-python-based-esb-solution-for-your-soa-5aef67114570
 
 
 
@@ -29,24 +39,6 @@ makeGetRequest("https://goo.gl/MV8Tkh");
 
 
 
-
-
-// const {
-//     extract 
-//   } = require('article-parser');
-   
-//   const url = 'https://goo.gl/MV8Tkh';
-   
-//   extract(url).then((article) => {
-//     const $ = cheerio.load(article.content);
-//     let h2 = $('h2').text();
-//     console.log(h2)
-//     let p = $('p').text();
-//     console.log(p)
-    
-//   }).catch((err) => {
-//     console.log(err);
-//   });
 
 
 
