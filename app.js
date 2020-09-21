6e9cb2ba8e5d146e9c72cfe8e0f8613ded1dee97
ShelbyE <60080830+Shelby219@users.noreply.github.com>
2020-09-21 17:00:13 +1000

    added class to dom

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const cheerio = require('cheerio')
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
 const myButton = document.getElementById("submit-button");
-
+const main = document.getElementById("main");
 // var matcher = /^(?:\w+:)?\/\/([^\s\.]+\.\S{2}|localhost[\:?\d]*)\S*$/;
 // function isUrl(string){
 //   return matcher.test(string);
@@ -31,11 +31,12 @@ async function makeGetRequest() {
   console.log(text2Speech) 
   const textArticle = document.createElement("div");
   const textnode = document.createTextNode(text2Speech); 
-  document.body.appendChild(textArticle);         // Create a text node
+  main.appendChild(textArticle);         // Create a text node
   textArticle.appendChild(textnode); 
   const playBut = document.createElement("BUTTON");
   playBut.innerHTML = "Play Me";  
-  document.body.appendChild(playBut); 
+  main.appendChild(playBut); 
+  playBut.classList.add("btn btn-lg btn-secondary");
   return text2Speech;
 }
 
